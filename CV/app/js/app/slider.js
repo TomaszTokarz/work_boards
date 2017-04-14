5b67caf9d2d81b0d3944bf0d07765e7d407978f6
TomaszTokarz <tomektokarz@tlen.pl>
2017-04-14 18:20:08 +0200

    CV: content, CSS

--- CV/app/js/app/slider.js
+++ CV/app/js/app/slider.js
@@ -4,10 +4,12 @@ var Slider = function(el, width, height) {
     this.$items = this.$el.find('.js_slider_item');
     this.$itemContainer = this.$el.find('.js_slider_items');
     this.$listItems = this.$el.find('.js_slider_list_item');
-    this.heights = [];
+    this.height = 0;
     this.currentSlide = 0;
+    this.autoTime = 3000;
+    this.clickTime = 10000;
 
-    this.slide = function(id) {
+    this.slide = function(id, time) {
         window.clearTimeout(this.timeout);
         for (var i = 0; i < this.$items.length; i++) {
             this.$items.eq(i).css({
@@ -20,22 +22,22 @@ var Slider = function(el, width, height) {
             if (this.currentSlide++ >= this.$items.length - 1) {
                 this.currentSlide = 0;
             }
-            this.slide(this.currentSlide);
-        }.bind(this), 3000);
+            this.slide(this.currentSlide, this.autoTime);
+        }.bind(this), time);
     }.bind(this);
 
     for (var i = 0; i < this.$items.length; i++) {
-        this.heights.push( this.$items.eq(i).height() );
+        this.height = Math.max( this.height, this.$items.eq(i).height())
     };
 
     this.$itemContainer.css({
-        height: Math.max( parseInt(this.heights) )
+        height: Math.max( parseInt(this.height) )
     });
 
     this.$listItems.click(function(e) {
         this.currentSlide = $(e.currentTarget).index()
-        this.slide( this.currentSlide );
+        this.slide( this.currentSlide,  this.clickTime);
     }.bind(this));
 
-    this.slide(this.currentSlide);
+    this.slide(this.currentSlide, this.autoTime);
 };
